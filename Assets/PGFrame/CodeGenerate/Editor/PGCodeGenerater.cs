b3845634529d3524f7cf01a9367c3c4c504bf8d8
Ethan <yishionwang314@163.com>
2016-10-22 13:48:01 +0800

    generate code 1@

---
+++ Assets/PGFrame/CodeGenerate/Editor/PGCodeGenerater.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using PogoTools;
+
+public class PGCodeGenerater
+{
+	public static readonly string lt = "PGFrame";
+	public static readonly Color lc = new Color (.7f, .5f, 1f);
+
+	FileInfo xViewModelBase;
+	FileInfo xViewModel;
+	FileInfo xControllerBase;
+	FileInfo xController;
+
+	public void Init ()
+	{
+		xViewModelBase = new FileInfo (Path.Combine (Application.dataPath, "PGFrame/CodeGenerate/Template/__XXX__ViewModelBase.txt"));
+		xViewModel = new FileInfo (Path.Combine (Application.dataPath, "PGFrame/CodeGenerate/Template/__XXX__ViewModel.txt"));
+		xControllerBase = new FileInfo (Path.Combine (Application.dataPath, "PGFrame/CodeGenerate/Template/__XXX__ControllerBase.txt"));
+		xController = new FileInfo (Path.Combine (Application.dataPath, "PGFrame/CodeGenerate/Template/__XXX__Controller.txt"));
+	}
+
+	public void GenerateCode (string workspaceName, string elementName)
+	{
+		IList<string> filesGenerated = new List<string> ();
+
+		GenerateViewModelBase (workspaceName, elementName, filesGenerated);
+		GenerateViewModel (workspaceName, elementName, filesGenerated);
+		GenerateControllerBase (workspaceName, elementName, filesGenerated);
+		GenerateController (workspaceName, elementName, filesGenerated);
+
+		PRDebug.TagLog (lt, lc, JsonConvert.SerializeObject (filesGenerated, Formatting.Indented));
+
+	}
+
+	void GenerateViewModelBase (string workspaceName, string elementName, IList<string> filesGenerated)
+	{
+		string targetPath = Path.Combine (Application.dataPath, "_Main/" + workspaceName + "/_Scripts/ViewModel/_Base");
+
+		string code = File.ReadAllText (xViewModelBase.FullName);
+		code = code.Replace ("__XXX__", elementName);
+
+		string file = Path.Combine (targetPath, string.Format ("{0}ViewModelBase.cs", elementName));
+		File.WriteAllText (file, code);
+
+		filesGenerated.Add (file);
+	}
+
+	void GenerateViewModel (string workspaceName, string elementName, IList<string> filesGenerated)
+	{
+		string targetPath = Path.Combine (Application.dataPath, "_Main/" + workspaceName + "/_Scripts/ViewModel");
+
+		string code = File.ReadAllText (xViewModel.FullName);
+		code = code.Replace ("__XXX__", elementName);
+
+		string file = Path.Combine (targetPath, string.Format ("{0}ViewModel.cs", elementName));
+		File.WriteAllText (file, code);
+
+		filesGenerated.Add (file);
+	}
+
+	void GenerateController (string workspaceName, string elementName, IList<string> filesGenerated)
+	{
+		string targetPath = Path.Combine (Application.dataPath, "_Main/" + workspaceName + "/_Scripts/Controller");
+
+		string code = File.ReadAllText (xController.FullName);
+		code = code.Replace ("__XXX__", elementName);
+
+		string file = Path.Combine (targetPath, string.Format ("{0}Controller.cs", elementName));
+		File.WriteAllText (file, code);
+
+		filesGenerated.Add (file);
+	}
+
+	void GenerateControllerBase (string workspaceName, string elementName, IList<string> filesGenerated)
+	{
+		string targetPath = Path.Combine (Application.dataPath, "_Main/" + workspaceName + "/_Scripts/Controller/_Base");
+
+		string code = File.ReadAllText (xControllerBase.FullName);
+		code = code.Replace ("__XXX__", elementName);
+
+		string file = Path.Combine (targetPath, string.Format ("{0}ControllerBase.cs", elementName));
+		File.WriteAllText (file, code);
+
+		filesGenerated.Add (file);
+	}
+
+	public void DeleteCode (string workspaceName, string elementName)
+	{
+		string[] targetPaths = {
+			Path.Combine (Application.dataPath, "_Main/" + workspaceName + "/_Scripts/Controller/" + string.Format ("{0}Controller.cs", elementName)),
+			Path.Combine (Application.dataPath, "_Main/" + workspaceName + "/_Scripts/Controller/_Base/" + string.Format ("{0}ControllerBase.cs", elementName)),
+			Path.Combine (Application.dataPath, "_Main/" + workspaceName + "/_Scripts/ViewModel/" + string.Format ("{0}ViewModel.cs", elementName)),
+			Path.Combine (Application.dataPath, "_Main/" + workspaceName + "/_Scripts/ViewModel/_Base/" + string.Format ("{0}ViewModelBase.cs", elementName)),
+		};
+
+		foreach (string s in targetPaths) {
+			File.Delete (s);
+		}
+	}
+}
---
+++ Assets/PGFrame/CodeGenerate/Editor/PGFrameWindow.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using PogoTools;
+using Newtonsoft.Json;
+
+public class PGFrameWindow : EditorWindow
+{
+	public static readonly string lt = "PGFrame";
+	public static readonly Color lc = new Color (.6f, .4f, 1f);
+
+	[MenuItem ("PogoRock/PGFrame...")]
+	static void Init ()
+	{
+		PGFrameWindow window = (PGFrameWindow)EditorWindow.GetWindow (typeof(PGFrameWindow));
+		window.Show ();
+	}
+
+	void OnGUI ()
+	{
+		GUILayout.Label ("PGFrame", EditorStyles.boldLabel);
+		if (GUILayout.Button ("刷新")) {
+			RefreshFiles ();
+		}
+		DesignList ();
+
+		if (GUILayout.Button ("发布代码")) {
+			Generator.GenerateCode ("WS1", "Second");
+			AssetDatabase.Refresh ();
+		}
+
+		if (GUILayout.Button ("删除代码")) {
+			Generator.DeleteCode ("WS1", "Second");
+			AssetDatabase.Refresh ();
+		}
+	}
+
+	FileInfo[] files;
+
+	PGCodeGenerater Generator;
+
+	void RefreshFiles ()
+	{
+		Generator = new PGCodeGenerater ();
+		Generator.Init ();
+
+		string[] fileNames = Directory.GetFiles (Path.Combine (Application.dataPath, "PGFrameDesign"), "*.xlsx");
+		files = new FileInfo[fileNames.Length];
+		for (int i = 0; i < fileNames.Length; i++) {
+			files [i] = new FileInfo (fileNames [i]);
+		}
+	}
+
+	void DesignList ()
+	{
+		GUILayout.BeginVertical ();
+
+		if (files != null) {
+			for (int i = 0; i < files.Length; i++) {
+				string fileName = files [i].Name;
+				EditorGUILayout.LabelField (fileName);
+			}
+		} else {
+			RefreshFiles ();
+		}
+
+		GUILayout.EndVertical ();
+	}
+
+
+}
